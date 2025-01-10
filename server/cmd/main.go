6364b0906149434a6ca97c511ed49fd3c475e3b2
TheDummyUser <abhiram.reddy122002@gmail.com>
2025-01-10 22:18:53 +0530

    removed standard lib moving to fiber dont ask why

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,42 +1,19 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	"log"
+import "github.com/gofiber/fiber/v2"
 
-	"github.com/TheDummyUser/server/cmd/api"
-	"github.com/TheDummyUser/server/config"
-	"github.com/TheDummyUser/server/db"
-	"github.com/go-sql-driver/mysql"
-)
-
-func main() {
-	db, err := db.NewMySQLStorage(mysql.Config{
-		User:                 config.Envs.DBUser,
-		Addr:                 config.Envs.DBAddress,
-		DBName:               config.Envs.DBName,
-		Net:                  "tcp",
-		AllowNativePasswords: true,
-		ParseTime:            true,
-	})
-
-	initStorage(db)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	config := api.NewServe(":8080", db)
-	if err := config.Run(); err != nil {
-		fmt.Print(err)
-	}
+type ReferenceJson struct {
+	Message string `json:"messahe"`
 }
 
-func initStorage(db *sql.DB) {
-	err := db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Print("sucessfully connected to database\n")
+func main() {
+	app := fiber.New()
+
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.JSON(
+			fiber.Map{"Message": "pong"},
+		)
+	})
+
+	app.Listen(":8080")
 }

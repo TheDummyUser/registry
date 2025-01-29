53315982a117139a92dfc0d5920961c50f89c13c
TheDummyUser <abhiram.reddy122002@gmail.com>
2025-01-29 23:49:36 +0530

    removed devenv moved repo to a submodule

--- devenv.nix
+++ devenv.nix
@@ -1,69 +1,9 @@
 {
+  config,
   pkgs,
   ...
 }:
-
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
-  packages = with pkgs; [
-    git
-    bun
-    nodejs
-    yarn
-    scrcpy
-  ];
-
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-  languages = {
-    go.enable = true;
-    javascript.enable = true;
-    typescript.enable = true;
-  };
-  # https://devenv.sh/processes/
-  processes.cargo-watch.exec = "cargo-watch";
-
-  android = {
-    enable = true;
-    reactNative.enable = true;
-    platforms.version = [ "35" ];
-    systemImageTypes = [ "google_apis_playstore" ];
-    abis = [
-      "arm64-v8a"
-      "x86_64"
-    ];
-    platformTools.version = "35.0.2";
-    ndk.enable = true;
-    buildTools.version = [
-      "34.0.0"
-      "35.0.0"
-    ];
-    emulator = {
-      enable = true;
-      version = "35.2.5";
-    };
-    sources.enable = false;
-    systemImages.enable = true;
-    googleAPIs.enable = true;
-    googleTVAddOns.enable = true;
-    extras = [ "extras;google;gcm" ];
-    extraLicenses = [
-      "android-sdk-preview-license"
-      "android-googletv-license"
-      "android-sdk-arm-dbt-license"
-      "google-gdk-license"
-      "intel-android-extra-license"
-      "intel-android-sysimage-license"
-      "mips-android-sysimage-license"
-    ];
-    # android-studio = pkgs.android-studio;
-  };
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
   services.mysql = {
     enable = true;
     initialDatabases = [
@@ -76,7 +16,7 @@
       {
         name = "root";
         ensurePermissions = {
-          "justadb.*" = "ALL PRIVILEGES";
+          "testdb.*" = "ALL PRIVILEGES";
         };
       }
       {
@@ -88,30 +28,4 @@
     ];
   };
 
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
--- server/go.mod
+++ server/go.mod
@@ -7,7 +7,7 @@ require github.com/go-sql-driver/mysql v1.8.1
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.6 // indirect
+	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
